3b611a00682a87efd067b75dec9c86fe725b6e3e
Christine Lee <chrstn.lee@mail.utoronto.ca>
2020-01-12 02:16:49 -0500

    Modified UI for account registration.

--- budget-buddy/app/src/main/java/com/example/budgetbuddy/MainActivity.java
+++ budget-buddy/app/src/main/java/com/example/budgetbuddy/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.budgetbuddy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Intent intent = getIntent();
-        String username = intent.getStringExtra("username");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    Intent intent = getIntent();
+    String username = intent.getStringExtra("username");
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
