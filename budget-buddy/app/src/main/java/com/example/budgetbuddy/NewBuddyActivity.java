4e4d035ce7c829c11431aae8ad8e4a66f9e76149
Christine Lee <chrstn.lee@mail.utoronto.ca>
2020-01-12 05:02:19 -0500

    Added imports to NewBuddyActivity.

--- budget-buddy/app/src/main/java/com/example/budgetbuddy/NewBuddyActivity.java
+++ budget-buddy/app/src/main/java/com/example/budgetbuddy/NewBuddyActivity.java
@@ -1,14 +1,12 @@
 package com.example.budgetbuddy;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-public class NewBuddyActivity extends AppCompatActivity
-    implements AdapterView.OnItemSelectedListener {
+
+public class NewBuddyActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +17,10 @@ public class NewBuddyActivity extends AppCompatActivity
     String buddyUserName = buddyText.getText().toString();
 
     TextView addBuddy = (TextView) findViewById(R.id.buddy_status);
-    if (LoginActivity.accounts.get(buddyUserName)!= null){
-      addBuddy.setText("You are now " + buddyUserName + "'s Buddy!" )
+    if (LoginActivity.accounts.get(buddyUserName) != null) {
+      addBuddy.setText("You are now " + buddyUserName + "'s Buddy!");
     } else {
-      addBuddy.setText("Buddy not found! Please enter a new username.")
+      addBuddy.setText("Buddy not found! Please enter a new username.");
     }
   }
-
-
 }
