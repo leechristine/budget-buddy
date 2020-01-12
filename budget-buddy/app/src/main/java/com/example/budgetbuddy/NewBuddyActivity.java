ce85d60b98ee42ee4139521795b138c748b4eabe
Sonya Cao <sonyacao@Sonyas-MacBook-Air.local>
2020-01-12 03:43:22 -0500

    new buddy

---
+++ budget-buddy/app/src/main/java/com/example/budgetbuddy/NewBuddyActivity.java
@@ -0,0 +1,29 @@
+package com.example.budgetbuddy;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class TransactionActivity extends AppCompatActivity
+    implements AdapterView.OnItemSelectedListener {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.transaction);
+
+
+
+    TextView addBuddy = (TextView) findViewById(R.id.buddy_status);
+    if (LoginActivity.accounts.get(buddy)!= null){
+      addBuddy.setText("You are now " + )
+    } else {
+      return false;
+    }
+  }
+
+
+}
